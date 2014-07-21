e380e001c171122e30c09d4e1276cdbebfb1cc60
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-07-21 13:04:02 +1000

    Episode 34 Complete

--- app/main_controller.rb
+++ app/main_controller.rb
@@ -6,7 +6,27 @@ class MainController < UIViewController
 
     add_hello_button
 
-    # setup toolbar here
+    # navigationController.toolbarHidden = false
+    view.addSubview(@toolbar = UIToolbar.new)
+    @toolbar.frame = CGRectMake(0, 150, 320, 50)
+    @toolbar.setItems([
+      UIBarButtonItem.alloc.initWithTitle(
+        "Hello",
+        style: UIBarButtonItemStylePlain,
+        target: self,
+        action: 'hello_pressed:'
+      ),
+      UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+        UIBarButtonSystemItemFlexibleSpace,
+        target: nil,
+        action: nil
+      ),
+      UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+        UIBarButtonSystemItemAction,
+        target: self,
+        action: 'hello_pressed:'
+      )
+    ])
   end
 
   def hello_pressed(sender)
